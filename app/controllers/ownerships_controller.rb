d545b666fafdee30c84f034e2fd8c0bc5962a1ae
namitop <nariyasuhidetaka@gmail.com>
2016-05-22 21:53:51 +0000

    forget to switch branch on rakuten_items

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,7 +11,10 @@ class OwnershipsController < ApplicationController
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
     if @item.new_record?
       # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
-      items = {}
+      items = RakutenWebService::Ichiba::Item.search(
+        keyword: params[:q],
+        imageFlag: 1,
+      )
 
       item                  = items.first
       @item.title           = item['itemName']
@@ -25,16 +28,33 @@ class OwnershipsController < ApplicationController
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
+    @item = Item.find(params[:item_id])
+    if params[:type]=='Have'
+      current_user.have(@item)
+    end
+    if params[:type]=='Want'
+      current_user.want(@item)
+    end
+    
     
 
   end
 
   def destroy
-    @item = Item.find(params[:item_id])
+    @item = Item.find(params[:item_id]) #何これ？
 
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
+    if params[:type]=='Have'
+      @item = current_user.haves.find(params[:id]).item
+      current_user.unhave(@item)
+    end
+    if params[:type]=='Want'
+      @item = current_user.wants.find(params[:id]).item
+      current_user.unwant(@item)
+    end
+    
 
   end
 end
