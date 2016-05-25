706b1f76ba187b854a0b2b60f888cd18b7ec95a6
namitop <nariyasuhidetaka@gmail.com>
2016-05-25 21:13:07 +0000

    (ranking) 2times submit task

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -7,7 +7,9 @@ class RankingController < ApplicationController
     @have_array = Array.new()
     #have_array = @items.sort {|a, b| a[:item_id].want_users.count <=> a[:id].want_users.count }
     @items.each do |item|
-      @have_array.push([item ,item.have_users.count])
+      if item.have_users.count > 0
+        @have_array.push([item ,item.have_users.count])
+      end
     end
     @have_array.sort! { |a, b| b[1] <=> a[1] }
   end
@@ -15,7 +17,9 @@ class RankingController < ApplicationController
   def want
     @want_array = Array.new()
     @items.each do |item|
-      @want_array.push([item ,item.want_users.count])
+      if item.want_users.count > 0
+        @want_array.push([item ,item.want_users.count])
+      end
     end
     @want_array.sort! { |a, b| b[1] <=> a[1] }
   end
